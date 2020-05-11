e75e35d427bd61c28cd5b3163542a08fcab24811
dhsystembr <64677570+dhsystembr@users.noreply.github.com>
2020-05-11 11:32:17 -0300

    asdasd

--- src/test/java/prod/HealthCheck.java
+++ src/test/java/prod/HealthCheck.java
@@ -21,11 +21,10 @@ public class HealthCheck {
 
 		cap.setPlatform(Platform.LINUX);
 		WebDriver drive = new RemoteWebDriver(new URL("http://192.168.48.138:4444/wd/hub"), cap);
-		String version;
 		try {
 			drive.navigate().to("http://192.168.48.138:9999/tasks");
 			drive.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-			version = drive.findElement(By.id("version")).getText();
+			String version = drive.findElement(By.id("version")).getText();
 			Assert.assertTrue(version.startsWith("build"));
 			System.out.println(version);
 		} finally {
